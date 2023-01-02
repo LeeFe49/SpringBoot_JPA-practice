a72cb185dfa73c29a53a1adb9926153267154e9f
Cheol Lee <chullee49@naver.com>
2023-01-02 18:49:42 +0900

    사용자 조건 검색 요청 * JPA  문자열 포함 여부 탐색 쿼리 * 응답 결과 {header / data} 형식 반환 -> 해당 양식의 클래스 생성

---
+++ src/main/java/com/example/practice/user/model/ResponseMessage.java
@@ -0,0 +1,46 @@
+package com.example.practice.user.model;
+
+import com.example.practice.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseMessage {
+
+    private ResponseMessageHeader header;
+    private Object data;
+
+    public static ResponseMessage fail(String message) {
+        return ResponseMessage.builder()
+                .header(ResponseMessageHeader.builder()
+                        .result(false)
+                        .resultCode("")
+                        .message(message)
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .build())
+                .data(null)
+                .build();
+    }
+
+    public static ResponseMessage success(Object data) {
+        return ResponseMessage.builder()
+                .header(ResponseMessageHeader.builder()
+                        .result(true)
+                        .resultCode("")
+                        .message("")
+                        .status(HttpStatus.OK.value())
+                        .build())
+                .data(data)
+                .build();
+    }
+}
