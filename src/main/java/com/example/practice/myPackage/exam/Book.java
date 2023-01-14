f8b85276ffc4b2c7ed6cedd8cbf7b137b129374a
Cheol Lee <chullee49@naver.com>
2023-01-14 21:00:52 +0900

    test

---
+++ src/main/java/com/example/practice/myPackage/exam/Book.java
@@ -0,0 +1,28 @@
+package com.example.practice.myPackage.exam;
+
+public class Book {
+
+    private String name;
+    private long price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public Book(String name, long price) {
+        this.name = name;
+        this.price = price;
+    }
+}
